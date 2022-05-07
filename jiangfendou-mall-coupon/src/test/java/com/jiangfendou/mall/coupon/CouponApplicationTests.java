e073d48fdda464e569e8685ae2ad147132877b8d
jiangmh <minghong.jiang@grandchallenge.co.jp>
2022-05-07 20:59:46 +0800

    项目初始化

---
+++ jiangfendou-mall-coupon/src/test/java/com/jiangfendou/mall/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangfendou.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
