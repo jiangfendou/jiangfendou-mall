a73ba7b7df8e1d93278e9273881efb32271c3aff
jiangmh <minghong.jiang@grandchallenge.co.jp>
2022-08-17 09:22:00 +0800

    删除access key

--- jiangfendou-mall-third-part/src/test/java/com/jiangfendou/mall/thirdpart/ThirdPartApplicationTests.java
+++ jiangfendou-mall-third-part/src/test/java/com/jiangfendou/mall/thirdpart/ThirdPartApplicationTests.java
@@ -21,8 +21,6 @@ public class ThirdPartApplicationTests {
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
 //        String endpoint = "oss-cn-beijing.aliyuncs.com";
 //        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-//        String accessKeyId = "LTAI5t8soJUfN6MDpALdHcmQ";
-//        String accessKeySecret = "1jIUteCIrfbIRxlt1jhm4R5zlfL0oP";
 //        // 填写Bucket名称，例如examplebucket。
         String bucketName = "jiangfendou";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
