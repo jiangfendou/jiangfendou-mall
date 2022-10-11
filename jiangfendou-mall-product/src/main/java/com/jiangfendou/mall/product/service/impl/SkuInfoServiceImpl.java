a9aa30cbf42ee5173625546cb5550d7adbebdf13
jiangmh <minghong.jiang@grandchallenge.co.jp>
2022-10-11 10:07:19 +0800

    no message

--- jiangfendou-mall-product/src/main/java/com/jiangfendou/mall/product/service/impl/SkuInfoServiceImpl.java
+++ jiangfendou-mall-product/src/main/java/com/jiangfendou/mall/product/service/impl/SkuInfoServiceImpl.java
@@ -2,6 +2,8 @@ package com.jiangfendou.mall.product.service.impl;
 
 import com.jiangfendou.mall.product.entity.SpuInfoEntity;
 import com.jiangfendou.mall.product.vo.SpuSaveVo;
+import java.math.BigDecimal;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -21,11 +23,37 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<SkuInfoEntity> wrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if(!StringUtils.isEmpty(key)) {
+            wrapper.and((w) -> {
+                w.eq("id", key).or().like("spu_name", key);
+            });
+        }
+        String min = (String) params.get("min");
+        if(!StringUtils.isEmpty(min)) {
+            wrapper.ge("price", min);
+        }
+        String max = (String) params.get("max");
+        if(!StringUtils.isEmpty(min)) {
+            BigDecimal maxBig = new BigDecimal(max);
+            if (maxBig.compareTo(new BigDecimal("0")) == 1) {
+                wrapper.le("price", max);
+            }
+        }
+        String brandId = (String) params.get("brandId");
+        if(!StringUtils.isEmpty(brandId) && !"0".equalsIgnoreCase(brandId)) {
+            wrapper.eq("brand_id", brandId);
+        }
+        String catelogId = (String) params.get("catelogId");
+        if(!StringUtils.isEmpty(catelogId) && !"0".equalsIgnoreCase(catelogId)) {
+            wrapper.eq("catalog_id", catelogId);
+        }
         IPage<SkuInfoEntity> page = this.page(
             new Query<SkuInfoEntity>().getPage(params),
-            new QueryWrapper<SkuInfoEntity>()
+            wrapper
         );
-
         return new PageUtils(page);
     }
 
